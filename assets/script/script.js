7533508c8de14b43729701e322ffa64026da8bfb
William Rocha <williamferreira313@gmail.com>
2021-04-28 09:13:15 -0300

    Addicted the mobile menu

---
+++ assets/script/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+document.querySelector(".mobile-menu").addEventListener("click", function () {
+  if (document.querySelector(".menu nav ul").style.display == "flex") {
+    document.querySelector(".menu nav ul").style.display = "none";
+  } else {
+    document.querySelector(".menu nav ul").style.display = "flex";
+  }
+});
